897bdc63e323b62412e14185d22338ea8a9a8b3c
wilmerbaby <wilmerbaby@gmail.com>
2020-09-29 16:32:26 -0400

    made api call

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-var APIkey = "0b7e120ecb6a0831c512bd4a23e5b882";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
-  APIKey +
-  "units=imperial";
+$(document).ready(function(){
+  
+  var APIkey = "0b7e120ecb6a0831c512bd4a23e5b882";
+  
+  function searchWeather(town){
+    var queryURL ="http://api.openweathermap.org/data/2.5/weather?q=" + town + "&appid=" + APIkey + "&units=imperial";
+    $.ajax({
+      url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response)
+    
+     $(".myCity").text("City:" + response.name);
+     $(".temp").text("Temperture:" + response.main.temp);
+     $(".humid").text("Humidity:" + response.main.humidity);
+    // $(".temperture").text("response.main.temp");
+  });
+  }
+  
+  
+  
+  $("#idbutton").on("click", function(e){
+    e.preventDefault
+    var searchTown = $("#searchCity").val();
+    console.log(searchTown)
+  searchWeather(searchTown)
+})
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  $(".city").text("City name" + "response.name");
-  $(".wind").text("response.wind.speed");
-  $(".humidity").text("response.main.humidity");
-  $(".temperture").text("response.main.temp");
-});
+
+
+
+})
