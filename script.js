5a5fd0a043668ac06c0a3790b95fc1b414df9291
wilmerbaby <wilmerbaby@gmail.com>
2020-10-02 15:52:54 -0400

    added readme, deleted code not in use

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-$(document).ready(function(){
-  
+$(document).ready(function () {
   var APIkey = "0b7e120ecb6a0831c512bd4a23e5b882";
-  
-  function searchWeather(town){
-    var queryURL ="http://api.openweathermap.org/data/2.5/weather?q=" + town + "&appid=" + APIkey + "&units=imperial";
+
+  function searchWeather(town) {
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      town +
+      "&appid=" +
+      APIkey +
+      "&units=imperial";
     $.ajax({
       url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response)
-    
-     $(".myCity").text("City:" + response.name);
-     $(".temp").text("Temperture:" + response.main.temp);
-     $(".humid").text("Humidity:" + response.main.humidity);
-    // $(".temperture").text("response.main.temp");
-  });
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+
+      $(".myCity").text("Weather In:" + response.name);
+      $(".temp").text("Temperture:" + response.main.temp);
+      $(".humid").text("Humidity:" + response.main.humidity);
+      $(".wind").text("Wind Speed:" + response.main.temp);
+      $(".uv").text("UV Index:");
+    });
   }
-  
-  
-  
-  $("#idbutton").on("click", function(e){
-    e.preventDefault
+
+  $("#idbutton").on("click", function (e) {
+    e.preventDefault;
     var searchTown = $("#searchCity").val();
-    console.log(searchTown)
-  searchWeather(searchTown)
-})
+    console.log(searchTown);
+    searchWeather(searchTown);
+  });
+});
 
-
-
-
-})
+console.log(town);
