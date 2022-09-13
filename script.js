65953a11e0fd88861adda31b27f6e77372b02789
Wilmer Rivera <wilmerbaby@gmail.com>
2022-09-13 12:27:53 -0400

    started local storage feature

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  var APIkey = "0b7e120ecb6a0831c512bd4a23e5b882";
+  var APIkey = "3c5008effeceb13ebf5b25bfb8e0b11a";
 
   function searchWeather(town) {
+    
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       town +
@@ -13,7 +14,13 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
+
       console.log(response);
+
+      
+        
+
+      
 
       $(".myCity").text("Weather In:" + response.name);
       $(".temp").text("Temperture:" + response.main.temp);
@@ -22,13 +29,78 @@ $(document).ready(function () {
       $(".uv").text("UV Index:");
     });
   }
+  
+
 
   $("#idbutton").on("click", function (e) {
+
     e.preventDefault;
     var searchTown = $("#searchCity").val();
-    console.log(searchTown);
+    var pastCitys = []
+    
+   
+  //  if(localStorage.getItem("history") === null){
+  //   localStorage.setItem("history",JSON.stringify([searchTown]))
+    
+  //  }else{
+  //   localStorage.push()
+  //  }
+      
+     
+   function setToLocalStorage(text){
+    // if(localStorage.getItem("history") === null)
+  //  var localStorageHistory = localStorage.getItem("history");
+  //  console.log(localStorageHistory)
+    
+  //   localStorageHistory.push(text)
+  //   console.log(localStorageHistory)
+    
+    // localStorage.setItem("history",JSON.stringify(pastCitys))
+  if(localStorage.getItem("history") === null){
+    localStorage.setItem("history","[]")
+
+
+  }else{
+    pastCitys = JSON.parse(localStorage.getItem("history"));
+    console.log(pastCitys);
+    pastCitys.push(text);
+    console.log(pastCitys);
+    localStorage.setItem("history",JSON.stringify(pastCitys));
+    console.log(pastCitys);
+
+  
+
+  }
+
+ 
+  
+
+   }
+   
+    
+    // console.log(searchTown);
+   
     searchWeather(searchTown);
+    setToLocalStorage(searchTown)
+    
+    makeHistoryListBtn(searchTown);
+    
+
+    
+    
+   
   });
 });
 
-console.log(town);
+
+
+function makeHistoryListBtn(text){
+  
+  let historyLi = $("<li>");
+  let historyButtn = $("<button>").addClass("Cbtn");
+  historyButtn.text(text)
+
+  historyLi.append(historyButtn)
+  $("#ulSearches").append(historyLi)
+  $("#searchCity").val("");
+}
